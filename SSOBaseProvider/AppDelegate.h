04fd8de6887bea565d2d1aee1b619a62451949bf
Gabriel Cartier <gcartier@samsaodev.com>
2015-04-17 17:50:05 -0400

    Initial commit

---
+++ SSOBaseProvider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SSOBaseProvider
+//
+//  Created by Gabriel Cartier on 2015-04-17.
+//  Copyright (c) 2015 Samsao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SSOBaseProvider/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SSOBaseProvider
+//
+//  Created by Gabriel Cartier on 2015-04-17.
+//  Copyright (c) 2015 Samsao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
