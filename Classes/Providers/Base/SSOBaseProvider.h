133464bcc1688d54f19ca0e9521dd2c0e259266b
Gabriel Cartier <gcartier@samsaodev.com>
2015-04-21 14:57:51 -0400

    Added a simple provider to allow a collection view and table view with a simple array displayed  * Took the delegate out and put it in a .h file for easier reuse

--- Classes/Providers/Base/SSOBaseProvider.h
+++ Classes/Providers/Base/SSOBaseProvider.h
@@ -1,22 +1,21 @@
 //
 //  SSOBaseProvider.h
-//
+//  SSOBaseProvider
 //
 //  Created by Gabriel Cartier on 2015-04-14.
-//
+//  Copyright (c) 2015 Samsao. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import "SSCellViewItem.h"
 #import "SSCellViewSection.h"
 #import "SSBaseViewCellProtocol.h"
-
-@protocol SSOBaseProviderDelegate;
+#import "SSOProviderDelegate.h"
 
 @interface SSOBaseProvider : NSObject
 
-@property(strong, nonatomic) NSMutableArray *inputData;
-@property(weak, nonatomic) id<SSOBaseProviderDelegate> delegate;
+@property(weak, nonatomic) NSMutableArray *inputData;
+@property(weak, nonatomic) id<SSOProviderDelegate> delegate;
 
 /**
  *  Get the object data from an index path. This will get the object data of an item
@@ -44,28 +43,5 @@
  *  @return the index path of the object
  */
 - (NSIndexPath *)indexPathForObject:(id)object;
-
-@end
-
-//@FIXME: Needs cleaning
-@protocol SSOBaseProviderDelegate <NSObject>
-
-@optional
-
-/**
- *  Overide the tableViewDelegate method
- *
- *  @param provider  the provider
- *  @param indexPath the indexPath selected
- */
-- (void)provider:(SSOBaseProvider *)provider didSelectRowAtIndexPath:(NSIndexPath *)indexPath;
-
-/**
- *  Overide the tableViewDelegate method
- *
- *  @param provider  the provider
- *  @param indexPath the indexPath selected
- */
-- (void)provider:(SSOBaseProvider *)provider didDeselectRowAtIndexPath:(NSIndexPath *)indexPath;
 
 @end
