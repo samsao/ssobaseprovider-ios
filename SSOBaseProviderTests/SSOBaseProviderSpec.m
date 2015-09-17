1f8e172168667f7504c4b7d915590927b79301c9
Guilherme Lisboa <glisboa@samsaodev.com>
2015-09-17 15:56:07 -0400

    Adding mock code to testing

--- SSOBaseProviderTests/SSOBaseProviderSpec.m
+++ SSOBaseProviderTests/SSOBaseProviderSpec.m
@@ -10,6 +10,7 @@
 #import <XCTest/XCTest.h>
 #import <Specta/Specta.h>
 #import <Expecta.h>
+#import <OCMock.h>
 #import "SSOBaseProvider.h"
 #import "SSOProviderItem.h"
 #import "SSOProviderSection.h"
@@ -18,80 +19,153 @@
 NSString *const kTestRI = @"";
 NSString *const kTestNib = @"";
 
-SpecBegin(SSOBaseProvider) describe(@"Base Provider", ^{
-  // Creating Data
+SpecBegin(SSOBaseProvider)
+    //     describe(@"Base Provider", ^{
+    //      // Creating Data
+    //
+    //      SSOTestModel *td = [SSOTestModel new];
+    //
+    //      SSOProviderItem *pi = [SSOProviderItem newProviderItemWithData:td reusableIdentifier:kTestRI cellNibName:kTestNib onBundleOrNil:nil];
+    //      SSOProviderSection *providerSection = [SSOProviderSection newSectionWithData:@[ pi ]];
+    //
+    //      SSOBaseProvider *provider = [SSOBaseProvider newProviderWithData:@[ providerSection ] andDelegate:nil];
+    //
+    //      expect(provider).toNot.beNil();
+    //      expect(provider).toNot.beNull();
+    //
+    //      context(@"Insertion", ^{
+    //
+    //        SSOTestModel *m1 = [SSOTestModel new];
+    //        SSOTestModel *m2 = [SSOTestModel new];
+    //        SSOTestModel *m3 = [SSOTestModel new];
+    //
+    //        SSOProviderItem *a = [SSOProviderItem newProviderItemWithData:m1 reusableIdentifier:kTestRI cellNibName:kTestNib onBundleOrNil:nil];
+    //        SSOProviderItem *b = [SSOProviderItem newProviderItemWithData:m2 reusableIdentifier:kTestRI cellNibName:kTestNib onBundleOrNil:nil];
+    //        SSOProviderItem *c = [SSOProviderItem newProviderItemWithData:m3 reusableIdentifier:kTestRI cellNibName:kTestNib onBundleOrNil:nil];
+    //        NSInteger section = 0;
+    //        SSOProviderSection *currentSection = provider.allSections[section];
+    //
+    //        it(@"Add objects", ^{
+    //          NSArray *data = @[ a, b ];
+    //
+    //          BOOL wasAdded = [provider addObjectsToProviderData:data inSection:section];
+    //          if (wasAdded) {
+    //              expect(currentSection.sectionItems).to.contain(a);
+    //              expect(currentSection.sectionItems).to.contain(b);
+    //          } else {
+    //              expect(currentSection.sectionItems).toNot.contain(a);
+    //              expect(currentSection.sectionItems).toNot.contain(b);
+    //          }
+    //
+    //    });
+    //    it(@"Add object at index", ^{
+    //      NSInteger indexToAdd = 0;
+    //      BOOL wasAdded = [provider addObject:c atIndexPath:[NSIndexPath indexPathForRow:indexToAdd inSection:section]];
+    //      if (wasAdded) {
+    //          expect(currentSection.sectionItems).to.contain(c);
+    //          expect([provider sectionAtIndex:section].sectionItems[indexToAdd]).to.equal(c);
+    //      } else {
+    //          expect(currentSection.sectionItems).toNot.contain(c);
+    //      }
+    //    });
+    //
+    //    it(@"remove objects", ^{
+    //      NSArray *data = @[ a, b ];
+    //      [provider addObjectsToProviderData:data inSection:section];
+    //
+    //      NSArray *removedIndexes = [provider removeObjectsFromProvider:data inSection:section];
+    //      if (removedIndexes) {
+    //          expect(currentSection.sectionItems).toNot.contain(a);
+    //          expect(currentSection.sectionItems).toNot.contain(b);
+    //          expect(currentSection.sectionItems).to.contain(c);
+    //      } else {
+    //          expect(currentSection.sectionItems).to.contain(a);
+    //          expect(currentSection.sectionItems).to.contain(b);
+    //          expect(currentSection.sectionItems).to.contain(c);
+    //      }
+    //
+    //    });
+    //    it(@"Remove single object", ^{
+    //      NSInteger removedIndex = [provider removeObjectFromProvider:c inSection:section];
+    //      if (removedIndex != -1) {
+    //          expect(currentSection.sectionItems).toNot.contain(c);
+    //      } else {
+    //          expect(currentSection.sectionItems).to.contain(c);
+    //      }
+    //    });
+    //#warning TODO: ADD Test for ADD/Remove Sections
+    //  });
+    //});
 
-  SSOTestModel *td = [SSOTestModel new];
+    describe(@"Base Provider", ^{
+      // Creating Data
 
-  SSOProviderItem *pi = [SSOProviderItem newProviderItemWithData:td reusableIdentifier:kTestRI cellNibName:kTestNib onBundleOrNil:nil];
-  SSOProviderSection *providerSection = [SSOProviderSection newSectionWithData:@[ pi ]];
+      SSOProviderItem *pi = OCMClassMock([SSOProviderItem class]);
 
-  SSOBaseProvider *provider = [SSOBaseProvider newProviderWithData:@[ providerSection ] andDelegate:nil];
+      SSOProviderSection *providerSection = OCMClassMock([SSOProviderSection class]);
+      OCMStub([providerSection sectionItems]).andReturn(@[ pi ]);
 
-  expect(provider).toNot.beNil();
-  expect(provider).toNot.beNull();
+      SSOBaseProvider *provider = [SSOBaseProvider newProviderWithData:@[ providerSection ] andDelegate:nil];
 
-  context(@"Insertion", ^{
+      expect(provider).toNot.beNil();
+      expect(provider).toNot.beNull();
 
-    SSOTestModel *m1 = [SSOTestModel new];
-    SSOTestModel *m2 = [SSOTestModel new];
-    SSOTestModel *m3 = [SSOTestModel new];
+      context(@"Insertion", ^{
 
-    SSOProviderItem *a = [SSOProviderItem newProviderItemWithData:m1 reusableIdentifier:kTestRI cellNibName:kTestNib onBundleOrNil:nil];
-    SSOProviderItem *b = [SSOProviderItem newProviderItemWithData:m2 reusableIdentifier:kTestRI cellNibName:kTestNib onBundleOrNil:nil];
-    SSOProviderItem *c = [SSOProviderItem newProviderItemWithData:m3 reusableIdentifier:kTestRI cellNibName:kTestNib onBundleOrNil:nil];
-    NSInteger section = 0;
-    SSOProviderSection *currentSection = provider.allSections[section];
+        SSOProviderItem *a = OCMClassMock([SSOProviderItem class]);
+        SSOProviderItem *b = OCMClassMock([SSOProviderItem class]);
+        SSOProviderItem *c = OCMClassMock([SSOProviderItem class]);
+        NSInteger sectionIndex = 0;
+        NSArray *data = @[ a, b ];
 
-    it(@"Add objects", ^{
-      NSArray *data = @[ a, b ];
+        it(@"Add objects", ^{
 
-      BOOL wasAdded = [provider addObjectsToProviderData:data inSection:section];
-      if (wasAdded) {
-          expect(currentSection.sectionItems).to.contain(a);
-          expect(currentSection.sectionItems).to.contain(b);
-      } else {
-          expect(currentSection.sectionItems).toNot.contain(a);
-          expect(currentSection.sectionItems).toNot.contain(b);
-      }
+          OCMStub([providerSection addItemsToSection:data]).andReturn(YES);
 
-    });
-    it(@"Add object at index", ^{
-      NSInteger indexToAdd = 0;
-      BOOL wasAdded = [provider addObject:c atIndexPath:[NSIndexPath indexPathForRow:indexToAdd inSection:section]];
-      if (wasAdded) {
-          expect(currentSection.sectionItems).to.contain(c);
-          expect([provider sectionAtIndex:section].sectionItems[indexToAdd]).to.equal(c);
-      } else {
-          expect(currentSection.sectionItems).toNot.contain(c);
-      }
-    });
+          BOOL wasAdded = [provider addObjectsToProviderData:data inSection:sectionIndex];
 
-    it(@"remove objects", ^{
-      NSArray *data = @[ a, b ];
-      [provider addObjectsToProviderData:data inSection:section];
+          expect(wasAdded).to.beTruthy();
+          OCMVerify([providerSection addItemsToSection:data]);
 
-      NSArray *removedIndexes = [provider removeObjectsFromProvider:data inSection:section];
-      if (removedIndexes) {
-          expect(currentSection.sectionItems).toNot.contain(a);
-          expect(currentSection.sectionItems).toNot.contain(b);
-          expect(currentSection.sectionItems).to.contain(c);
-      } else {
-          expect(currentSection.sectionItems).to.contain(a);
-          expect(currentSection.sectionItems).to.contain(b);
-          expect(currentSection.sectionItems).to.contain(c);
-      }
+          wasAdded = [provider addObjectsToProviderData:data inSection:-1];
 
-    });
-    it(@"Remove single object", ^{
-      NSInteger removedIndex = [provider removeObjectFromProvider:c inSection:section];
-      if (removedIndex != -1) {
-          expect(currentSection.sectionItems).toNot.contain(c);
-      } else {
-          expect(currentSection.sectionItems).to.contain(c);
-      }
-    });
+          expect(wasAdded).to.beFalsy();
+
+        });
+        it(@"Add object at index", ^{
+
+          NSInteger indexToAdd = 0;
+
+          OCMStub([providerSection addItemToSection:c atIndex:indexToAdd]).andReturn(YES);
+
+          BOOL wasAdded = [provider addObject:c atIndexPath:[NSIndexPath indexPathForRow:indexToAdd inSection:sectionIndex]];
+
+          expect(wasAdded).to.beTruthy();
+          OCMVerify([providerSection addItemToSection:c atIndex:indexToAdd]);
+
+        });
+
+        it(@"remove objects", ^{
+          [provider addObjectsToProviderData:data inSection:sectionIndex];
+          OCMStub([providerSection removeItemsFromSection:data]).andReturn(@[ @(1)]);
+            
+            
+            
+          NSArray *removedIndexes = [provider removeObjectsFromProvider:data inSection:sectionIndex];
+          expect(removedIndexes).toNot.beNil();
+
+          
+
+        });
+//        it(@"Remove single object", ^{
+//          NSInteger removedIndex = [provider removeObjectFromProvider:c inSection:section];
+//          if (removedIndex != -1) {
+//              expect(currentSection.sectionItems).toNot.contain(c);
+//          } else {
+//              expect(currentSection.sectionItems).to.contain(c);
+//          }
+//        });
 #warning TODO: ADD Test for ADD/Remove Sections
-  });
-});
+      });
+    });
 SpecEnd
